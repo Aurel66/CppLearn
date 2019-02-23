de3866065b45784deae2dae5456b8c2eddd11dd9
Aurel66 <unlocker929@gmaul.com>
2019-02-23 13:16:01 +0300

    comments removed

--- CppLearn/CppLearn.cpp
+++ CppLearn/CppLearn.cpp
@@ -7,17 +7,14 @@ using namespace std;
 
 int main()
 {
-	int i; // счетчик цикла
+	int i; 
 	setlocale(0, "");
 	int mas[] = { 1,5,4,3,3 };
 	int dob = 1;
-	for (i = 0; i < 5; i++) // задаем начальное значение 1, конечное 1000 и задаем шаг цикла - 1.
+	for (i = 0; i < 5; i++) 
 	{
 		dob = dob * mas[i];
 	}
 	cout << "Dob: " << dob << endl;
 	return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
