355085fa4b51f6a1cc16df6d4359b3dce9f704cc
Aurel66 <unlocker929@gmaul.com>
2019-02-23 12:53:12 +0300

    project init

--- CppLearn/CppLearn.cpp
+++ CppLearn/CppLearn.cpp
@@ -3,19 +3,20 @@
 
 #include "pch.h"
 #include <iostream>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n"; 
+	int i; // счетчик цикла
+	int sum = 0; // сумма чисел от 1 до 1000.
+	setlocale(0, "");
+	for (i = 1; i <= 1000; i++) // задаем начальное значение 1, конечное 1000 и задаем шаг цикла - 1.
+	{
+		sum = sum + i;
+	}
+	cout << "Сумма чисел от 1 до 1000 = " << sum << endl;
+	return 0;
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
